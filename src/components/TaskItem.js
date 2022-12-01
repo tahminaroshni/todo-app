720d810a2d86047b3e3149b89f1a6ae8bcfbbafd
tahminaroshni <tahminatanni12@gmail.com>
2022-12-01 18:44:14 +0600

    checked operation added

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { FiEdit, FiTrash2 } from "react-icons/fi";
 import { DeletedIdContext, EditedIdContext, EditedTextContext } from '../App';
 
@@ -8,12 +8,16 @@ const TaskItem = ({ addedTask, editedText, setEditedText }) => {
   const handleEditBtn = useContext(EditedIdContext);
   const handleEditedTextSubmitter = useContext(EditedTextContext);
 
+  const [isChecked, setIsChecked] = useState(false);
+
   return (
     <div className='w-full mx-auto p-5 bg-slate-800 flex justify-between mb-3 rounded hover:bg-gradient-to-r from-cyan-900 to-slate-800'>
       <div className='flex gap-3'>
-        <input type="checkbox" />
+        <input type="checkbox" checked={isChecked} onChange={(e) => setIsChecked(!isChecked)} />
         {
-          !addedTask.isEditable ? <p className='text-slate-200'>{addedTask.text}</p> :
+          !addedTask.isEditable ? (
+            !isChecked ? <p className='text-slate-200'>{addedTask.text}</p> : <p className='line-through text-cyan-400'>{addedTask.text}</p>
+          ) :
             <form onSubmit={(e) => handleEditedTextSubmitter(e, addedTask.id)}>
               <input
                 value={editedText}
@@ -21,7 +25,6 @@ const TaskItem = ({ addedTask, editedText, setEditedText }) => {
                 type="text" className='bg-transparent pb-1 border-b-2 border-cyan-600 outline-none text-cyan-100' />
             </form>
         }
-        {/* <p className='text-slate-200'>{addedTask.text}</p> */}
       </div>
       <div className='flex gap-3 duration-300'>
         <button onClick={() => handleEditBtn(addedTask.id)}>
